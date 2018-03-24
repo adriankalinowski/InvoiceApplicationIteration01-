7ee82daf9c1b648eba6475ff3332bcc7453f4914
Adrian <kalinow15@gmail.com>
2018-03-24 00:14:32 -0400

    added database schema and temp home page

---
+++ database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS InvoiceApplication;
+
+CREATE TABLE IF NOT EXISTS Broker
+(
+	company_name VARCHAR(200),
+	address VARCHAR(),
+	city VARCHAR(),
+	state VARCHAR(),
+	zip_code INTEGER,
+	email VARCHAR(),
+	bill_via_email VARCHAR()
+);
+
+CREATE TABLE IF NOT EXISTS Invoices
+(
+	invoiceId INTEGER AUTO_INCREMENT,
+	shipped_from_city VARCHAR(),
+	shipped_from_state VARCHAR(),
+	shipped_to_city VARCHAR(),
+	shipped_to_state VARCHAR(),
+	amount DOUBLE,
+	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	broker VARCHAR,
+	FOREIGN KEY ()
+);
