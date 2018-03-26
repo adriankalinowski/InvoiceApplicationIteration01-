116b7608e0b618fb7df66580e87b5c7fc6807624
Adrian <kalinow15@gmail.com>
2018-03-25 20:46:45 -0400

    added many to many relationship between invoices and brokers

--- database.sql
+++ database.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS InvoiceApplication;
 
 CREATE TABLE IF NOT EXISTS Broker
 (
+	broker_id INTEGER AUTO_INCREMENT,
 	company_name VARCHAR(200),
 	address VARCHAR(),
 	city VARCHAR(),
@@ -9,18 +10,25 @@ CREATE TABLE IF NOT EXISTS Broker
 	zip_code INTEGER,
 	email VARCHAR(),
 	bill_via_email VARCHAR(),
-	PRIMARY KEY (company_name)
+	PRIMARY KEY (broker_id)
 );
 
 CREATE TABLE IF NOT EXISTS Invoices
 (
-	invoiceId INTEGER AUTO_INCREMENT,
+	invoice_id INTEGER AUTO_INCREMENT,
 	shipped_from_city VARCHAR(),
 	shipped_from_state VARCHAR(),
 	shipped_to_city VARCHAR(),
 	shipped_to_state VARCHAR(),
 	amount DOUBLE,
-	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	broker VARCHAR,
-	FOREIGN KEY (broker) REFERENCES Broker(company_name)
+	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	PRIMARY KEY (invoice_id)
+);
+
+CREATE TABLE IF NOT EXISTS Invoice_Broker
+(
+	invoice_id INTEGER,
+	broker_id INTEGER,
+	FOREIGN KEY (invoice_id) REFERENCES Invoices(invoice_id),
+	FOREIGN KEY (broker_id) REFERENCES Broker(broker_id)
 );
