3cdb94c6ab1228d012f55048e8bb05701d3f76a3
Adrian <kalinow15@gmail.com>
2018-04-24 23:03:19 -0400

    reworked html form to be more dynamic and gave it a cleaner look

--- createInvoice.php
+++ createInvoice.php
@@ -1,5 +1,5 @@
 <?php
-require_once("config.php");
+/*require_once("config.php");
 $db=config["db"];
 
 $conn = mysqli_connect($db["host"], $db["user"], $db["pass"]);
@@ -13,7 +13,7 @@ if($conn){
 $sql = "INSERT INTO Invoices (shipped_from_city, shipped_from_state, shipped_to_city, shipped_to_state, amount) VALUES (?,?,?,?,?)";
 
 $stmt = mysqli_prepare($sql);
-$stmt->bind_param("ssssd",$_POST['FromCity'],$_POST['FromState'],$_POST['ToCity'],$_POST['ToState'],$_POST['Amount']);
+$stmt->bind_param("ssssd",$_POST['fromCity'],$_POST['fromState'],$_POST['toCity'],$_POST['toState'],$_POST['amount']);
 $stmt->execute();
 
 $invoiceId = mysqli_insert_id($conn);
@@ -35,8 +35,8 @@ $stmt = mysqli_prepare($sql);
 $stmt->bind_param("ii",$invoiceId, $brokerId);
 $stmt->execute();
 
-$stmt->close();*/
-$conn->close();
+$stmt->close();
+$conn->close();*/
 ?>
 
 <!DOCTYPE html>
@@ -45,10 +45,101 @@ $conn->close();
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="print.css" type="text/css" media="print">
 </head>
 <body>
-<?php
-echo $_POST['amount'];
-?>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="home.html">Kali Enterprises Inc</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="home.html">Home</a></li>
+                <li><a href="createInvoice.html">Create Invoice</a></li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" action="/action_page.php">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search Invoices">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </nav>
+
+        <div class="page-header">
+            <div class="pull-left">
+                <h2>Kali Enterprises</h2>
+                <h6>
+                    42275 June Drive <br>
+                    Sterling Heights, MI 48314
+                </h6>
+                <h6>
+                    Phone: (586) 344-6945 <br>
+                    Email: kalienterpriseinc@gmail.com
+                </h6>
+            </div>
+            <div class="pull-right">
+                <h2 class="text-right">INVOICE</h2>
+                <h6 class="text-right">
+                    Invoice #:
+                    <?php
+                        echo "####"
+                    ?>
+                    <br>
+                    Date:
+                    <?php
+                        echo "##/##/####"
+                    ?>
+                </h6>
+                <h6 class="text-right"></h6>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+        <h4>
+            Reference #:
+            <?php
+                echo $_POST['reference'];
+            ?>
+        </h4>
+
+        <br>
+
+        <h4>Shipped From:</h4>
+        <?php
+            echo $_POST['fromCity'] . ", " . $_POST['fromState'];
+        ?>
+
+        <br><br>
+
+        <h4>Shipped To:</h4>
+        <?php
+            echo $_POST['toCity'] . ", " . $_POST['toState'];
+        ?>
+
+        <br><br>
+
+        <h4>
+            Amount Due:
+            <?php
+                echo "$" . $_POST['amount'];
+            ?>
+        </h4>
+
+        <br><br><br>
+
+        <h4>Remit Payment To:</h4>
+        <p>
+            Kali Enterprises Inc <br>
+            42275 June Drive <br>
+            Sterling Heights, MI 48314 <br>
+        </p>
+
+        <div class="panel-footer">
+            <h5 class="text-center">Thank you for your business!</h5>
+        </div>
+
+    </div>
 </body>
 </html>
